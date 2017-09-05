e9a568a59230b6c117babde676a6954bf58ac461
ruithumbup <rui.li.roy@gmail.com>
2017-09-04 22:18:13 -0700

    Update model.py

--- model.py
+++ model.py
@@ -72,9 +72,9 @@ model.add(Lambda(lambda x: x/127.5 - 1.,
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 # 5 convlution layers
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
